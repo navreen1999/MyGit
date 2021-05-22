105540cdc2b211daa859dbe8f10d5fb510d206cc
unknown <navreenabubacker@gmail.com>
2021-05-22 09:05:03 -0700

    Add All Files

---
+++ PersonalMVC/Controllers/PersonalController.cs
@@ -0,0 +1,71 @@
+﻿using EmployeeMVC.Models;
+using EmployeeMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMVC.Controllers
+{
+    public class PersonalController : Controller
+    {
+        private IRepo<Personal> _repo;
+        private ILogger<PersonalController> _logger;
+
+        public PersonalController(IRepo<Personal> repo,ILogger<PersonalController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+
+        }
+        public IActionResult Index()
+        {
+            List<Personal> personals = _repo.GetAll().ToList();
+            return View(personals);
+         
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Personal personal)
+        {
+            _repo.Add(personal);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Personal personal= _repo.Get(id);
+            return View(personal);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Personal personal)
+        {
+            _repo.Update(id, personal);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            Personal personal = _repo.Get(id);
+            return View(personal);
+        }
+        
+        public IActionResult Delete(int id) 
+        {
+            Personal personal = _repo.Get(id);
+            return View(personal);
+        }
+        [HttpPost
+            ]
+        public IActionResult Delete(Personal personal)
+        {
+            _repo.Delete(personal);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PersonalMVC/Migrations/20210521220851_NewMigration-Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeMVC.Migrations
+{
+    public partial class NewMigrationInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Qualification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsEmployed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoticePeriod = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentCTC = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personals", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Personals",
+                columns: new[] { "Id", "CurrentCTC", "IsEmployed", "Name", "NoticePeriod", "Qualification" },
+                values: new object[] { 1, 350000, "Yes", "Navreen", "21 May,2019", "Engineer" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personals");
+        }
+    }
+}
